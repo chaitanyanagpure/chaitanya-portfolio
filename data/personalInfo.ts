b40a3dd48046ce6914110e7b3453f1797108c901
chaitanyanagpure <chaitanyanagpure64791@gmail.com>
2026-07-15 17:27:09 +0530

    updated resume

--- data/personalInfo.ts
+++ data/personalInfo.ts
@@ -24,7 +24,7 @@ export const personalInfo: PersonalInfo = {
   location: "Maharashtra, India",
   github: "https://github.com/chaitanyanagpure",
   linkedin: "https://www.linkedin.com/in/chaitanyanagpure",
-  resumeUrl: "https://drive.google.com/file/d/14U4Hr9f5KY8K2dengVnPO0hYUxamj_e9/view?usp=drive_link",
+  resumeUrl: "https://drive.google.com/file/d/14DUkLHZFXTkOOK-8HgbUfiaesz_Fgm98/view?usp=drive_link",
   specialties: [
     "Machine Learning",
     "Deep Learning",
